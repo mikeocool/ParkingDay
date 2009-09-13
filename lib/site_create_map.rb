5192f323d053491ead00e64c3e97c051414209fd
Mike O'Toole <mikeo@mobile-166-217-211-110.mycingular.net>
2009-09-13 16:57:48 -0400

    adding some specs

--- lib/site_create_map.rb
+++ lib/site_create_map.rb
@@ -1,7 +1,12 @@
 require File.dirname(__FILE__)+'/input_map'
+require File.dirname(__FILE__)+'/site'
 
 module ParkingDay
   class SiteCreateMap < InputMap
+    model Site
+    
+    property :slug
+    
     def validate_input
       if ! @data[:slug].match(/^[A-Za-z0-9_\-]+$/)
         set_error(:slug, 'Slug must be alphanumeric.')
@@ -9,7 +14,9 @@ module ParkingDay
     end
   
     def do_save
-    
+      record = self.class.model.new
+      record.attributes = @data
+      return record
     end
   end
 end
--- spec/specs/site_create_map_spec.rb
+++ spec/specs/site_create_map_spec.rb
@@ -4,6 +4,14 @@ require File.dirname(__FILE__)+'/../../lib/site_create_map'
 include ParkingDay
 
 describe SiteCreateMap do
+  it "should accept valid data" do
+    data = {:slug => 'a_slug'}
+    map = SiteCreateMap.new
+    site = map.save(data)
+    site.slug.should == 'a_slug'
+    
+  end
+  
   it "should ensure that slug is alphanumeric" do
     data = {:slug => 'Hello how are you?'}
     map = SiteCreateMap.new
